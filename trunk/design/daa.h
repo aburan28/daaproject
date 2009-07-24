5ab0e95af62f01bd51f76de497d35c21e41e0238
bokuanghust <bokuanghust@847384e6-69f3-11de-bdd1-8505e4066020>
2009-07-24 13:08:06 +0000

    modify daa.h ss_issuer.h

--- trunk/design/daa.h
+++ trunk/design/daa.h
@@ -16,6 +16,13 @@
 
 typedef EC_POINT ECC_POINT;
 
+// bi_ptr2  mean  a+bi
+typedef struct tdbi_ptr2
+{
+	bi_ptr a;
+	bi_ptr b;
+}bi_ptr2;
+
 typedef struct tdECCParmeter
 {
 
@@ -38,62 +45,69 @@ typedef struct tdECCParmeter
 
 typedef struct tdIssuerPk
 {
-    ECC_PARAMETER * Eccparmeter;
-    BYTE * CapitalX;
-    UINT32 CapitalXLength;
-    BYTE * CapitalY;
-    UINT32 CapitalYLength;
+    ECC_PARAMETER  Eccparmeter;
+
+    ECC_POINT  CapitalX;
+
+    ECC_POINT  CapitalY;
+
 }TSS_DAA_ISSUER_PK;
 
 // issuer 私钥
 
 typedef struct tdIssuerSk
 {
-    BYTE * x;
-    UINT32 xLength;
-    BYTE * y;
-    UINT32 yLength;
+    bi_ptr x;
+
+    bi_ptr y;
+
 }TSS_DAA_ISSUER_SK;
 
 //issuer密钥
 
 typedef struct tdIssuerKey
 {
-    TSS_DAA_ISSUER_PK * IssuerPK;
-    TSS_DAA_ISSUER_SK * IssuerSK;
+    TSS_DAA_ISSUER_PK IssuerPK;
+
+    TSS_DAA_ISSUER_SK IssuerSK;
+
 }TSS_DAA_ISSUER_KEY;
 
 // issuer proof 验证 issuer 公钥的正确性
 
 typedef struct tdIssuerProof
 {
-    BYTE * CapitalXPrime;
-    UINT32 CapitalXPrimeLength;
-    BYTE * CapitalYPrime;
-    UINT32 CapitalYPrimeLength;
+	ECC_POINT CapitalXPrime;
+
+    ECC_POINT CapitalYPrime;
+
 }TSS_DAA_ISSUER_PROOF;
 
 // issuer session 存储会话临时值
 
-//TODO bi_ptr
+
 typedef struct tdIssuerJionSession
 {
-    BYTE * IssuerNone;
-    UINT32 IssuerNOneLength;
-    BYTE * CapitalF;
-    UINT32 CapitalFLength;
+    bi_ptr  IssuerNone;
+
+    ECC_POINT  CapitalF;
+
+    bi_ptr  ch;
+
+    bi_ptr  s;
+
 }TSS_DAA_ISSUER_JOIN_SESSION;
 
 //issuer credential 证书内容
 
 typedef struct tdCredential
 {
-    BYTE * CapitalA;
-    UINT32 CapitalALength;
-    BYTE * CapitalB;
-    UINT32 CapitalBLength;
-    BYTE * CapitalC;
-    UINT32 CapitalCLength;
+	ECC_POINT CapitalA;
+
+	ECC_POINT CapitalB;
+
+	ECC_POINT CapitalC;
+
 }TSS_DAA_CREDENTIAL;
 
 // TPM
@@ -101,45 +115,40 @@ typedef struct tdCredential
 typedef struct tdTPMSession
 {
     bi_ptr f;
-    //BYTE * CapitalF;
-    //UINT32 CapitalFLength;
-    //BYTE * ch;
-    //UINT32 chLength;
-    //BYTE * s;
-    //UINT32 slength;
+
 }TSS_DAA_TPM_JOIN_SESSION;
 
 // HOST
 
-typedef struct tdHostJoinSession
+typedef struct  tdHostJoinSession
 {
-    BYTE * Roa;
-    UINT32 RoaLength;
-    BYTE * Rob;
-    UINT32 RobLength;
-    BYTE * Roc;
-    UINT32 RocLength;
+    bi_ptr2 Roa;
+
+    bi_ptr2 Rob;
+
+    bi_ptr2 Roc;
+
 }TSS_DAA_HOST_JOIN_SESSION;
 
 // signature
 
 typedef struct tdSignature
 {
-    BYTE * CapitalAprime;
-    UINT32 CapitalAprimeLength;
-    BYTE * CapitalBPrime;
-    UINT32 CapitalBPrimeLength;
-    BYTE * CapitalCPrime;
-    UINT32 CapitalCPrimeLength;
-    BYTE * CapitalEPrime;
-    UINT32 CapitalEPrimeLength;
-    BYTE * ch;
-    UINT32 chLength;
-    BYTE * s;
-    UINT32 sLength;
-    BYTE * nv;
-    UINT32 nvLrngth;
-    BYTE * nt;
-    UINT32 ntLength;
+	ECC_POINT CapitalAprime;
+
+	ECC_POINT CapitalBPrime;
+
+	ECC_POINT CapitalCPrime;
+
+	ECC_POINT CapitalEPrime;
+
+	bi_ptr ch;
+
+	bi_ptr s;
+
+	bi_ptr nv;
+
+	bi_ptr nt;
+
 }TSS_DAA_SIGNNATURE;
 #endif
--- trunk/design/ss_issuer.h
+++ trunk/design/ss_issuer.h
@@ -7,19 +7,21 @@
 #define SS_ISSUER_H
 #include "daa.h"
 
-int TSS_DAA_JOIN_issuer_setup(UINT32 SecureLength,
-                              TSS_DAA_ISSUER_KEY * IssuerKey,
-                              TSS_DAA_ISSUER_PROOF IssuerProof);
+// Built issuer parameters
+int TSS_DAA_JOIN_issuer_setup(
+                              TSS_DAA_ISSUER_KEY *   IssuerKey,
+                              TSS_DAA_ISSUER_PROOF * IssuerProof);
 
-//TODO check
-int TSS_DAA_JOIN_issuer_int(BYTE * PlatformEndorsemenPubKey,
-                            UINT32 PlatformEndorsemenPubkeyLength,
-                            TSS_DAA_ISSUER_PK * IssuerPK,
-                            TSS_DAA_ISSUER_JOIN_SESSION * IssuerJoinSession,
-                            BYTE **  EncryptedNonceOfIssuer,
-                            UINT32 * EncryptedNonceOfIssuerLength);
 
-int TSS_DAA_JOIN_issuer_credentia(TSS_DAA_TPM_JOIN_SESSION * TpmJoinSession,
+int TSS_DAA_JOIN_issuer_init(
+							BYTE * 					  	  PlatformEndorsemenPubKey,
+                            UINT32 					  	  PlatformEndorsemenPubkeyLength,
+                            bi_ptr 						  EncryptedNonceOfIssuer,
+                            TSS_DAA_ISSUER_PK * 		  IssuerPK,
+                            TSS_DAA_ISSUER_JOIN_SESSION * IssuerJoinSession);
+
+
+int TSS_DAA_JOIN_issuer_credentia(TSS_DAA_ISSUER_JOIN_SESSION * TpmJoinSession,
                                   TSS_DAA_CREDENTIAL * Credential,
                                   BYTE **  EncyptedCred,
                                   UINT32 * EncyptedCredLength);
