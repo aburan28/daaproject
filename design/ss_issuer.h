ef9ed432adf55ae9c133815b87c3ec00140ea9a6
bokuanghust <bokuanghust@847384e6-69f3-11de-bdd1-8505e4066020>
2009-07-31 11:11:05 +0000

    finish the ss_tpm[1] and ss_issuer[2]

--- design/ss_issuer.h
+++ design/ss_issuer.h
@@ -7,10 +7,26 @@
 #define SS_ISSUER_H
 #include "daa.h"
 
+#ifndef RSA_NO_PADDING
+#define RSA_NO_PADDING		3
+#endif
 
-#define RSA_NO_PADDING				       3
+#ifndef EVP_DigestFinal_OUT_SIZE
 #define EVP_DigestFinal_OUT_SIZE   		   20
+#endif
+
+#ifndef DAA_PARAM_MESSAGE_DIGEST_ALGORITHM
 #define DAA_PARAM_MESSAGE_DIGEST_ALGORITHM "SHA1"
+#endif
+
+#ifndef RSA_MODLE_LENGTH
+#define RSA_MODLE_LENGTH 2048
+#endif
+
+#ifndef Kk
+#define Kk "islab"
+#endif
+
 // Built issuer parameters
 int TSS_DAA_JOIN_issuer_setup(
                               TSS_DAA_ISSUER_KEY *   IssuerKey,
@@ -20,14 +36,14 @@ int TSS_DAA_JOIN_issuer_setup(
 int TSS_DAA_JOIN_issuer_init(
 							BYTE * 					  	  PlatformEndorsemenPubKey,
                             UINT32 					  	  PlatformEndorsemenPubkeyLength,
-                            TSS_DAA_ISSUER_PK * 		  IssuerPK,
+                  //          TSS_DAA_ISSUER_PK * 		  IssuerPK,    Not use
                             TSS_DAA_ISSUER_JOIN_SESSION * IssuerJoinSession,
                             BYTE   ** 	   				  EncryptedNonceOfIssuer,
                             UINT32 *					  EncryptedNonceOfIssuerLength);
 
 
-int TSS_DAA_JOIN_issuer_credentia(TSS_DAA_ISSUER_JOIN_SESSION * TpmJoinSession,
-		                          TSS_DAA_CREDENTIAL2 * Credential,
+int TSS_DAA_JOIN_issuer_credentia(TSS_DAA_ISSUER_JOIN_SESSION * IssuerJoinSession,
+                                  TSS_DAA_CREDENTIAL          * Credential,
                                   BYTE **  EncyptedCred,
                                   UINT32 * EncyptedCredLength);
 #endif
